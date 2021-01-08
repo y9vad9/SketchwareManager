188eec0eeb7320dd42a868b828c79d6d45653078
Neon <32961194+y9neon@users.noreply.github.com>
2021-01-08 21:34:29 +0200

    BlockBeanParser.kt marked as deprecated

--- src/main/kotlin/io/sketchware/collections/common/BlockBeanParser.kt
+++ src/main/kotlin/io/sketchware/collections/common/BlockBeanParser.kt
@@ -1,24 +1,21 @@
 package io.sketchware.collections.common
 
-import io.sketchware.collections.models.BlockBean
-import io.sketchware.serialize
-import java.util.regex.Matcher
+import io.sketchware.collections.models.CollectionItem
+import io.sketchware.project.data.BlockParser
 import java.util.regex.Pattern
 
+@Deprecated("Use BlockParser instead.",
+    ReplaceWith("BlockParser", "io.sketchware.project.data.BlockParser"))
 object BlockBeanParser {
     private const val regex = "\\{.*?\\}$(?:\\{|\\s|\\Z)"
-    private val pattern: Pattern = Pattern.compile(regex, Pattern.MULTILINE)
+    val pattern: Pattern = Pattern.compile(regex, Pattern.MULTILINE)
 
     /**
      * Parse input string and converts it to list using regex pattern.
      * @param string string of collection file's content.
+     * @deprecated Use BlockParser.parseArray(string) instead.
      */
-    fun parseAsArray(string: String): List<BlockBean> {
-        val matcher: Matcher = pattern.matcher(string)
-        val array = ArrayList<BlockBean>()
-        while (matcher.find()) {
-            array.add(matcher.group(0).serialize())
-        }
-        return array
-    }
+    @Deprecated("Use BlockParser.parseAsArray(string) instead.",
+        ReplaceWith("BlockParser.parseAsArray(string)", "io.sketchware.project.data.BlockParser"))
+    fun parseAsArray(string: String): List<CollectionItem> = BlockParser.parseAsArray(string)
 }
