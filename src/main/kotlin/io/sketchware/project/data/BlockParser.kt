5c1d2801ba08635fcd4fc8f131e91219b7354473
Neon <32961194+y9neon@users.noreply.github.com>
2021-01-10 00:31:08 +0200

    refactor + improves

---
+++ src/main/kotlin/io/sketchware/project/data/BlockParser.kt
@@ -0,0 +1,29 @@
+package io.sketchware.project.data
+
+import io.sketchware.collections.common.BlockBeanParser
+import io.sketchware.utils.serialize
+import io.sketchware.utils.toJson
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+
+object BlockParser {
+    private const val regex = "\\{.*?\\}$(?:\\{|\\s|\\Z)"
+    val pattern: Pattern = Pattern.compile(regex, Pattern.MULTILINE)
+
+    inline fun <reified T> parseAsArray(string: String): List<T> {
+        val matcher: Matcher = pattern.matcher(string)
+        val array = ArrayList<T>()
+        while (matcher.find()) {
+            array.add(matcher.group(0).serialize())
+        }
+        return array
+    }
+
+    inline fun <reified T> toSaveableValue(list: List<T>) {
+        var output = ""
+        list.forEach {
+            output += "\n${it.toJson()}"
+        }
+    }
+
+}
