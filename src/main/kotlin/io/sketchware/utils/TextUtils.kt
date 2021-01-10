a7a1dfd899682b9dd321833eb4098d9e4c42afd9
Neon <32961194+y9neon@users.noreply.github.com>
2021-01-10 21:04:58 +0200

    unnecessary functionality has been removed

--- src/main/kotlin/io/sketchware/utils/TextUtils.kt
+++ src/main/kotlin/io/sketchware/utils/TextUtils.kt
@@ -1,7 +1,5 @@
 package io.sketchware.utils
 
-
-internal fun String.indexOfOrNull(string: String) = indexOf(string).takeIf { it != -1 }
 internal fun String.replaceOrInsertAtTop(regex: Regex, replacement: String): String {
     val content = regex.replace(this, replacement)
     return if (this == content)
