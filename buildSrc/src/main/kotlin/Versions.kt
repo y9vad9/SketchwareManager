16bd06720c935fc803ac999fa97c350cdf8d4fbf
Neon <32961194+y9neon@users.noreply.github.com>
2021-04-16 13:24:02 +0300

    update: version changed dev-2.4.2 -> dev-2.4.3

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,3 +1,4 @@
 const val kotlinSerializationVer = "1.1.0"
 const val kotlinCoroutinesVersion = "1.4.3"
-const val libraryVersion = "dev-2.4.2"
+const val libraryVersion = "dev-2.4.3"
+const val kotlinVersion = "1.4.30"
